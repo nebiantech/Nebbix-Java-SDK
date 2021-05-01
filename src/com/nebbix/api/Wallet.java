09341219230298ec0ca447a56bb468ead01c83b7
Jevison Archibong <jevison7x@yahoo.com>
2021-05-01 12:50:41 +0100

    Updated the Wallet class.

--- src/com/nebbix/api/Wallet.java
+++ src/com/nebbix/api/Wallet.java
@@ -9,14 +9,96 @@
  * or visit www.xyneex.com if you need additional information or have any
  * questions.
  */
-
 package com.nebbix.api;
+
+import java.sql.Timestamp;
 
 /**
  *
  * @author Jevison7x
  * @since May 1, 2021 12:45:30 PM
  */
-public class Wallet {
+public class Wallet
+{
+    private int id;
+    private String name;
+    private double walletBalance;
+    private boolean locked;
+    private String type;
+    private Timestamp updatedAt;
+    private Timestamp createdAt;
 
+    public Wallet()
+    {
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public double getWalletBalance()
+    {
+        return walletBalance;
+    }
+
+    public void setWalletBalance(double walletBalance)
+    {
+        this.walletBalance = walletBalance;
+    }
+
+    public boolean isLocked()
+    {
+        return locked;
+    }
+
+    public void setLocked(boolean locked)
+    {
+        this.locked = locked;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    public Timestamp getUpdatedAt()
+    {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt)
+    {
+        this.updatedAt = updatedAt;
+    }
+
+    public Timestamp getCreatedAt()
+    {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt)
+    {
+        this.createdAt = createdAt;
+    }
 }
