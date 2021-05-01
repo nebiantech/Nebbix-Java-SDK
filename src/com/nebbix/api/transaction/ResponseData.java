903f139de90adf147600169de06f5b35ff48cda8
Jevison Archibong <jevison7x@yahoo.com>
2021-05-01 13:07:00 +0100

    Transaction stuff

---
+++ src/com/nebbix/api/transaction/ResponseData.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2018, Xyneex Technologies. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * You are not meant to edit or modify this source code unless you are
+ * authorized to do so.
+ *
+ * Please contact Xyneex Technologies, #1 Orok Orok Street, Calabar, Nigeria.
+ * or visit www.xyneex.com if you need additional information or have any
+ * questions.
+ */
+package com.nebbix.api.transaction;
+
+/**
+ *
+ * @author Jevison7x
+ * @since May 1, 2021 1:02:49 PM
+ */
+public class ResponseData
+{
+    private Transaction transaction;
+    private String address;
+    private String referenceCode;
+    private String expirationTime;
+    private String qrCode;
+
+    public ResponseData()
+    {
+    }
+
+    public Transaction getTransaction()
+    {
+        return transaction;
+    }
+
+    public void setTransaction(Transaction transaction)
+    {
+        this.transaction = transaction;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getReferenceCode()
+    {
+        return referenceCode;
+    }
+
+    public void setReferenceCode(String referenceCode)
+    {
+        this.referenceCode = referenceCode;
+    }
+
+    public String getExpirationTime()
+    {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(String expirationTime)
+    {
+        this.expirationTime = expirationTime;
+    }
+
+    public String getQrCode()
+    {
+        return qrCode;
+    }
+
+    public void setQrCode(String qrCode)
+    {
+        this.qrCode = qrCode;
+    }
+}
---
+++ src/com/nebbix/api/transaction/Transaction.java
@@ -0,0 +1,194 @@
+package com.nebbix.api.transaction;
+
+/*
+ * Copyright (c) 2018, Xyneex Technologies. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * You are not meant to edit or modify this source code unless you are
+ * authorized to do so.
+ *
+ * Please contact Xyneex Technologies, #1 Orok Orok Street, Calabar, Nigeria.
+ * or visit www.xyneex.com if you need additional information or have any
+ * questions.
+ */
+
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author Jevison7x
+ * @since May 1, 2021 12:54:21 PM
+ */
+public class Transaction
+{
+    private int id;
+    private String title;
+    private String memo;
+    private double amount;
+    private String status;
+    private String phone;
+    private String address;
+    private String ref;
+    private String customData;
+    private String createdByType;
+    private String type;
+    private String subType;
+    private Timestamp updatedAt;
+    private Timestamp createdAt;
+    private double dollarValue;
+
+    public Transaction()
+    {
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getMemo()
+    {
+        return memo;
+    }
+
+    public void setMemo(String memo)
+    {
+        this.memo = memo;
+    }
+
+    public double getAmount()
+    {
+        return amount;
+    }
+
+    public void setAmount(double amount)
+    {
+        this.amount = amount;
+    }
+
+    public String getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(String status)
+    {
+        this.status = status;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getRef()
+    {
+        return ref;
+    }
+
+    public void setRef(String ref)
+    {
+        this.ref = ref;
+    }
+
+    public String getCustomData()
+    {
+        return customData;
+    }
+
+    public void setCustomData(String customData)
+    {
+        this.customData = customData;
+    }
+
+    public String getCreatedByType()
+    {
+        return createdByType;
+    }
+
+    public void setCreatedByType(String createdByType)
+    {
+        this.createdByType = createdByType;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    public String getSubType()
+    {
+        return subType;
+    }
+
+    public void setSubType(String subType)
+    {
+        this.subType = subType;
+    }
+
+    public Timestamp getUpdatedAt()
+    {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt)
+    {
+        this.updatedAt = updatedAt;
+    }
+
+    public Timestamp getCreatedAt()
+    {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt)
+    {
+        this.createdAt = createdAt;
+    }
+
+    public double getDollarValue()
+    {
+        return dollarValue;
+    }
+
+    public void setDollarValue(double dollarValue)
+    {
+        this.dollarValue = dollarValue;
+    }
+}
